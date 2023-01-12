5978913ecfaf32467c1cd6f5f6d14ab6d4a5935b
volatile static <volatile_static@zju.edu.cn>
2023-01-12 10:24:06 +0800

    update: notifier

--- types/xpcom/notifier.d.ts
+++ types/xpcom/notifier.d.ts
@@ -1,18 +1,110 @@
+/// <reference path="../../internal.d.ts" />
+
 declare namespace _ZoteroTypes {
+  namespace Notifier {
+    type Event = (
+      'add' | 'modify' | 'delete' | 'move' | 'remove' | 'refresh' |
+      'redraw' | 'trash' | 'unreadCountUpdated' | 'index'
+    );
+    type Type = (
+      'collection' | 'search' | 'share' | 'share-items' | 'item' | 'file' |
+      'collection-item' | 'item-tag' | 'tag' | 'setting' | 'group' | 'trash' |
+      'bucket' | 'relation' | 'feed' | 'feedItem' | 'sync' | 'api-key' | 'tab'
+    );
+    type Notify = (event: Event, type: Type, ids: string[], extraData: anyObj) => void;
+    interface Queue {
+      id: string;
+      _queue: { [type in Type]: Array<{ ids: string[], data: anyObj }> };
+      size: number;
+      options: object;
+      new(options?: object): this;
+    }
+  }
   interface Notifier {
-    registerObserver: (
-      ref: { notify: Function },
-      types?: string[],
+    readonly EVENT_LEVEL_OPTIONS: ['autoSyncDelay', 'skipAutoSync'];
+
+    /**
+     * @param {Object} [ref] signature {notify: function(event, type, ids, extraData) {}}
+     * @param {Array} [types] a list of types of events observer should be triggered on
+     * @param {String} [id] an id of the observer used in debug output
+     * @param {Integer} [priority] lower numbers correspond to higher priority of observer execution
+     * @returns {string}
+     */
+    registerObserver(
+      ref: { notify: Notifier.Notify },
+      types?: Notifier.Type[],
       id?: string,
-      priority?: null
-    ) => string;
+      priority?: number
+    ): string;
+
     unregisterObserver(id: String): void;
-    trigger: (
-      event: string,
-      type: string,
+
+    /**
+    * Trigger a notification to the appropriate observers
+    *
+    * Possible values:
+    *
+    * 	event: 'add', 'modify', 'delete', 'move' ('c', for changing parent),
+    *		'remove' (ci, it), 'refresh', 'redraw', 'trash', 'unreadCountUpdated', 'index'
+    * 	type - 'collection', 'search', 'item', 'collection-item', 'item-tag', 'tag',
+    *		'group', 'relation', 'feed', 'feedItem'
+    * 	ids - single id or array of ids
+    *
+    * Notes:
+    *
+    * - If event queuing is on, events will not fire until commit() is called
+    * unless _force_ is true.
+    *
+    * - New events and types should be added to the order arrays in commit()
+    **/
+    trigger(
+      event: Notifier.Event,
+      type: Notifier.Type,
       ids: number | number[],
-      extraData?: any,
+      extraData?: anyObj,
       force?: boolean
-    ) => any;
+    ): Promise<void | true>;
+
+    /**
+     * Queue an event until the end of the current notifier transaction
+     *
+     * Takes the same parameters as trigger()
+     *
+     * @throws If a notifier transaction isn't currently open
+     */
+    queue(
+      event: Notifier.Event,
+      type: Notifier.Type,
+      ids: string[],
+      extraData: { [option in ('autoSyncDelay' | 'skipAutoSync')]: unknown },
+      queue: Notifier.Queue
+    ): void;
+
+    /**
+     * Begin queueing event notifications (i.e. don't notify the observers)
+     *
+     * Note: Be sure the matching commit() gets called (e.g. in a finally{...} block) or
+     * notifications will break until Firefox is restarted or commit(true)/reset() is called manually
+     *
+     * @param {String} [transactionID]
+     */
+    begin(transactionID?: boolean): void;
+
+    /**
+     * Send notifications for ids in the event queue
+     *
+     * @param {Zotero.Notifier.Queue|Zotero.Notifier.Queue[]} [queues] - One or more queues to use
+     *     instead of the internal queue
+     * @param {String} [transactionID]
+     */
+    commit(
+      queues?: Notifier.Queue | Notifier.Queue[],
+      transactionID?: boolean
+    ): Promise<void>;
+
+    /*
+     * Reset the event queue
+     */
+    reset(transactionID?: boolean): void;
   }
 }
