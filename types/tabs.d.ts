b5d1a14d8722c1bbba717005394643f40b2680bb
volatile-static <volatile_static@zju.edu.cn>
2023-01-12 15:08:24 +0800

    fix tab

--- types/tabs.d.ts
+++ types/tabs.d.ts
@@ -32,7 +32,7 @@ declare const Zotero_Tabs: {
     index?: number;
     select?: boolean;
     onClose?: Function;
-  }) => { id: string; container: HTMLElement };
+  }) => { id: string; container: XUL.Box };
   rename(id: string, title: string): void;
   updateLibraryTabIcon(): void;
   close(ids: string | string[]): void;
