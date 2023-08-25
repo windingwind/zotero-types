ceb3fbc6d5a93b1398030df91c15b09793c37c58
volatile static <volatile_static@zju.edu.cn>
2023-08-25 15:44:09 +0800

    fix promise

--- types/promise.d.ts
+++ types/promise.d.ts
@@ -1,16 +1,24 @@
 declare namespace Zotero {
   interface Promise<T = void> extends globalThis.Promise<T> {
     method(fn: Function): () => _ZoteroTypes.PromiseWithResolved<T>;
-    defer(): _ZoteroTypes.PromiseObject;
+    defer(): _ZoteroTypes.PromiseObject<T>;
     delay(timeout: number): globalThis.Promise<void>;
   }
 }
 
 declare namespace _ZoteroTypes {
-  interface PromiseObject {
-    promise: Promise<any>;
-    resolve(reason?: any): void;
-    reject(reason?: any): void;
+  interface PromiseObject<T> {
+    promise: Promise<T> & {
+      isCancelled: () => boolean;
+      isResolved: () => boolean;
+      isRejected: () => boolean;
+      isFulfilled: () => boolean;
+      isPending: () => boolean;
+      value: () => T;
+      reason: () => T;
+    };
+    resolve(value?: T): void;
+    reject(reason?: T): void;
   }
 
   class PromiseWithResolved<T> extends Promise<T> {
