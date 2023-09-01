79da6db7938d885d3556721e94faac14da1b0226
volatile static <volatile_static@zju.edu.cn>
2023-09-01 13:17:06 +0800

    add bluebird to promise

--- types/promise.d.ts
+++ types/promise.d.ts
@@ -1,27 +1,21 @@
 declare namespace Zotero {
-  interface Promise<T = void> extends globalThis.Promise<T> {
-    method(fn: Function): () => _ZoteroTypes.PromiseWithResolved<T>;
-    defer(): _ZoteroTypes.PromiseObject<T>;
-    delay(timeout: number): globalThis.Promise<void>;
+  interface Promise<T = void> extends _ZoteroTypes.Bluebird<T> {
+    method(fn: Function): () => _ZoteroTypes.Bluebird<T>;
+    defer(): _ZoteroTypes.DeferredPromise<T>;
   }
 }
 
 declare namespace _ZoteroTypes {
-  interface PromiseObject<T> {
-    promise: Promise<T> & {
-      isCancelled: () => boolean;
-      isResolved: () => boolean;
-      isRejected: () => boolean;
-      isFulfilled: () => boolean;
-      isPending: () => boolean;
-      value: () => T;
-      reason: () => T;
-    };
+  type Bluebird<T> = import("bluebird")<T>;
+
+  /**
+   * @deprecated
+   */
+  type PromiseObject = DeferredPromise<unknown>;
+
+  interface DeferredPromise<T> {
+    promise: Bluebird<T>;
     resolve(value?: T): void;
     reject(reason?: T): void;
-  }
-
-  class PromiseWithResolved<T> extends Promise<T> {
-    isResolved: boolean;
   }
 }
