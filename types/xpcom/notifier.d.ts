26742dca1f608c20fcb84ccc8e9a53eb6e59bddb
Northword <northword@outlook.com>
2024-02-21 16:25:01 +0800

    Add _ZoteroTypes.Notifier.ExtraData

--- types/xpcom/notifier.d.ts
+++ types/xpcom/notifier.d.ts
@@ -38,11 +38,16 @@ declare namespace _ZoteroTypes {
       | "tab"
       | "itemtree"
       | "itempane";
+    type ExtraData = anyObj & {
+      [ids: number]: anyObj;
+      skipAutoSync?: true;
+      autoSyncDelay?: true;
+    };
     type Notify = (
       event: Event,
       type: Type,
       ids: string[] | number[],
-      extraData: anyObj,
+      extraData: ExtraData
     ) => void | Promise<void>;
     interface Queue {
       id: string;
@@ -66,7 +71,7 @@ declare namespace _ZoteroTypes {
       ref: { notify: Notifier.Notify },
       types?: Notifier.Type[],
       id?: string,
-      priority?: number,
+      priority?: number
     ): string;
 
     unregisterObserver(id: String): void;
@@ -94,7 +99,7 @@ declare namespace _ZoteroTypes {
       type: Notifier.Type,
       ids: number | number[],
       extraData?: anyObj,
-      force?: boolean,
+      force?: boolean
     ): Promise<void | true>;
 
     /**
@@ -109,7 +114,7 @@ declare namespace _ZoteroTypes {
       type: Notifier.Type,
       ids: string[],
       extraData: { [option in "autoSyncDelay" | "skipAutoSync"]: unknown },
-      queue: Notifier.Queue,
+      queue: Notifier.Queue
     ): void;
 
     /**
@@ -131,7 +136,7 @@ declare namespace _ZoteroTypes {
      */
     commit(
       queues?: Notifier.Queue | Notifier.Queue[],
-      transactionID?: boolean,
+      transactionID?: boolean
     ): Promise<void>;
 
     /*
