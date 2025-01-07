670ab56a2660bb64463624502901632020293300
windingwind <33902321+windingwind@users.noreply.github.com>
2025-01-07 23:07:19 +0100

    fix: move Prefs outside _ZoteroTypes

--- types/xpcom/prefs.d.ts
+++ types/xpcom/prefs.d.ts
@@ -1,18 +1,25 @@
-declare namespace _ZoteroTypes {
-  interface Prefs {
+declare namespace Zotero {
+  namespace Prefs {
     /**
      * Retrieve a preference
      */
-    get(pref: string, global?: boolean): boolean | string | number | undefined;
+    function get(
+      pref: string,
+      global?: boolean,
+    ): boolean | string | number | undefined;
 
     /**
      * Set a preference
      */
-    set(pref: string, value: boolean | string | number, global?: boolean): any;
+    function set(
+      pref: string,
+      value: boolean | string | number,
+      global?: boolean,
+    ): any;
 
-    clear(pref: string, global?: boolean): void;
+    function clear(pref: string, global?: boolean): void;
 
-    resetBranch(exclude?: string[], branch?: string): void;
+    function resetBranch(exclude?: string[], branch?: string): void;
 
     /**
      * @param {String} name - Preference name; if not global, this is on the extensions.zotero branch
@@ -20,15 +27,15 @@ declare namespace _ZoteroTypes {
      * @param {Boolean} [global]
      * @return {Symbol} - Symbol to pass to unregisterObserver()
      */
-    registerObserver(name: string, handler: Function, global?: boolean): symbol;
+    function registerObserver(
+      name: string,
+      handler: Function,
+      global?: boolean,
+    ): symbol;
 
     /**
      * @param {Symbol} symbol - Symbol returned from registerObserver()
      */
-    unregisterObserver(symbol: symbol): void;
+    function unregisterObserver(symbol: symbol): void;
   }
-}
-
-declare namespace Zotero {
-  const Prefs: _ZoteroTypes.Prefs;
 }
