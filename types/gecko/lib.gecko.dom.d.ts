7101a1f6265debec9523e7b7475abfb668c03e36
v_s <1043064987@qq.com>
2025-04-21 10:56:43 +0800

    fix: lib.gecko.dom.d.ts

--- types/gecko/lib.gecko.dom.d.ts
+++ types/gecko/lib.gecko.dom.d.ts
@@ -9185,8 +9185,7 @@ declare var EventCounts: {
 interface EventHandler {
 }
 
-interface EventListenerOrEventListenerObject {
-}
+type EventListenerOrEventListenerObject = EventListener;
 
 interface EventSourceEventMap {
     "error": Event;
@@ -11225,7 +11224,17 @@ interface HTMLCanvasElement extends HTMLElement {
     mozPrintCallback: PrintCallback | null;
     width: number;
     captureStream(frameRate?: number): CanvasCaptureMediaStream;
-    getContext(contextId: string, contextOptions?: any): nsISupports | null;
+    /**
+     * Returns an object that provides methods and properties for drawing and manipulating images and graphics on a canvas element in a document. A context object includes information about colors, line widths, fonts, and other graphic parameters that can be drawn on a canvas.
+     * @param contextId The identifier (ID) of the type of canvas to create. Internet Explorer 9 and Internet Explorer 10 support only a 2-D context using canvas.getContext("2d"); IE11 Preview also supports 3-D or WebGL context using canvas.getContext("experimental-webgl");
+     *
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/API/HTMLCanvasElement/getContext)
+     */
+    getContext(contextId: "2d", options?: CanvasRenderingContext2DSettings): CanvasRenderingContext2D | null;
+    getContext(contextId: "bitmaprenderer", options?: ImageBitmapRenderingContextSettings): ImageBitmapRenderingContext | null;
+    getContext(contextId: "webgl", options?: WebGLContextAttributes): WebGLRenderingContext | null;
+    getContext(contextId: "webgl2", options?: WebGLContextAttributes): WebGL2RenderingContext | null;
+    getContext(contextId: string, options?: any): RenderingContext | null;
     toBlob(callback: BlobCallback, type?: string, encoderOptions?: any): void;
     toDataURL(type?: string, encoderOptions?: any): string;
     transferControlToOffscreen(): OffscreenCanvas;
