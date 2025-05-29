f7e5237e5cf069416b3458ef08d8a28fbc567ec4
v_s <1043064987@qq.com>
2025-05-29 16:54:05 +0800

    add `FileHandlers`

--- types/xpcom/fileHandlers.d.ts
+++ types/xpcom/fileHandlers.d.ts
@@ -1,6 +1,56 @@
+/// <reference path="data/item.d.ts" />
+
+declare namespace _ZoteroTypes {
+  namespace FileHandlers {
+    interface Location {
+      annotationID?: string;
+      pageIndex?: number;
+      position?: { value: string };
+    }
+
+    interface OpenParams {
+      location?: Location;
+      openInWindow?: boolean;
+    }
+
+    interface FileHandlerOpenOptions {
+      filePath: string;
+      location?: Location;
+      page?: number;
+    }
+
+    interface FileHandler {
+      name: RegExp;
+      fallback?: boolean;
+      open(
+        appPath: string | null,
+        options: FileHandlerOpenOptions,
+      ): Promise<void>;
+    }
+
+    interface Platform {
+      pdf: FileHandler[];
+      epub: FileHandler[];
+    }
+  }
+
+  interface FileHandlers {
+    open(item: Zotero.Item, params?: FileHandlers.OpenParams): Promise<boolean>;
+    _handlersMac: FileHandlers.Platform;
+    _handlersWin: FileHandlers.Platform;
+    _handlersLinux: FileHandlers.Platform;
+    _getSystemHandler(mimeType: string): string | false;
+    _getSystemHandlerWin(mimeType: string): string | false;
+    _getSystemHandlerPOSIX(mimeType: string): string | false;
+    _checkAndExecWithoutBlocking(
+      command: string,
+      args: string[],
+    ): Promise<void>;
+  }
+}
+
 declare namespace Zotero {
-  // TODO: add types
-  namespace FileHandlers {}
+  const FileHandlers: _ZoteroTypes.FileHandlers;
 
   namespace OpenPDF {
     /**
