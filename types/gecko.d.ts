a032531623e4190533340f38cf8fbb4116e84a88
Abdelrahman AL MAROUK <72821992+almarouk@users.noreply.github.com>
2025-07-22 13:52:49 +0200

    fix #79: Duplicate identifier 'EventListenerOrEventListenerObject' (#80)

--- types/gecko.d.ts
+++ types/gecko.d.ts
@@ -27,8 +27,6 @@ interface DOMStringMap {
   [name: string]: string | undefined;
 }
 
-interface EventListenerOrEventListenerObject extends EventListener {}
-
 interface EventSourceEventMap {
   /**
    * Returns an object that provides methods and properties for drawing and manipulating images and graphics on a canvas element in a document. A context object includes information about colors, line widths, fonts, and other graphic parameters that can be drawn on a canvas.
